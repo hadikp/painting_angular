aa0a88dc788727d23aac7619f941cd1d35ab8e82
hadikp <hp20001025@gmail.com>
2021-05-16 15:29:17 +0200

    routing works

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-menu',
@@ -10,6 +11,24 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(function(){
+      let $window=$(window);
+      let $htmlBody=$('html,body');
+      let $gorgetogomb=$('#nyil');
+      
+      const lap=200;
+      $window.scroll(function(){
+        let aktTav:number=$htmlBody.scrollTop();
+        if(aktTav >=lap){
+          $gorgetogomb.show(500);
+        }else $gorgetogomb.hide(100);
+      });
+      //gombnyomásra felugrik a tetejére
+      $gorgetogomb.click(function(){
+        $htmlBody.animate({scrollTop:0},500);
+       });
+     });
   }
+
 
 }
